74ce5a955a23a1106b111affadcc5906bacb9f12
Gabriel Ramirez Melgarejo <isc.gabriel.ramirezm@gmail.com>
2014-05-16 16:12:17 -0500

    cambio del nombre

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Surver App' });
+  res.render('index', { title: 'Survey App' });
 });
 
 module.exports = router;
